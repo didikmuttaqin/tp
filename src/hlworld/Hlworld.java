4e315e323ac25df237968dfd78c60fb3b2621537
ainunfauziyahbahary <ainunfauziyahbahary@gmail.com>
2015-04-07 06:40:53 +0700

    new commit

---
+++ src/hlworld/Hlworld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hlworld;
+
+/**
+ *
+ * @author Ainun
+ */
+public class Hlworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, saya didik. NIM saya 6306130030. Saya sangat menyukai praktikum modul 8 ini.");
+    }
+    
+}
